6d8e03b6a360078c282c1c190e835f3f1373661d
WashingtonWang <xiangyu1034.wang@gmail.com>
2017-07-16 23:00:39 +0800

    add kotlin language

---
+++ kotlinlearn/src/main/java/com/mycoding/KotlinlearnApplication.java
@@ -0,0 +1,12 @@
+package com.mycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinlearnApplication.class, args);
+	}
+}
