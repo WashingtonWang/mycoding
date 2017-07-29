1963a5e7ed8ae39688c7d21bd897d77cf80ad9c8
WashingtonWang <xiangyu1034.wang@gmail.com>
2017-07-29 09:26:09 +0800

    add new project javabase

---
+++ javabase/src/main/java/com/mycoding/javabase/JavabaseApplication.java
@@ -0,0 +1,12 @@
+package com.mycoding.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabaseApplication.class, args);
+	}
+}
--- javabase/src/test/java/com/mycoding/javabase/JavabaseApplicationTests.java
+++ javabase/src/test/java/com/mycoding/javabase/JavabaseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mycoding;
+package com.mycoding.javabase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KotlinlearnApplicationTests {
+public class JavabaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
-
 	}
 
 }
