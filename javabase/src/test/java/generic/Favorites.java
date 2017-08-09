2a0d3362d662a77c39c849cc32e65d9dc4e2f7a9
WashingtonWang <xiangyu1034.wang@gmail.com>
2017-08-09 22:04:39 +0800

    add enum eg

---
+++ javabase/src/test/java/generic/Favorites.java
@@ -0,0 +1,33 @@
+package generic;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Favorites {
+
+    private Map<Class<?>, Object> favorites  = new HashMap<Class<?>,Object>();
+
+
+    public <T> void putFavorites(Class<T> type,T instance){
+        if (type == null)
+            throw new NullPointerException("Type is null");
+        favorites.put(type, type.cast(instance));
+    }
+
+    public <T> T getFavorites(Class<T> type){
+        return type.cast(favorites.get(type));
+    }
+
+    static Annotation getAnnotation(AnnotatedElement element, String annotationTypeName){
+        Class<?> annotationType = null;
+        try{
+            annotationType = Class.forName(annotationTypeName);
+        } catch (Exception e){
+            throw new IllegalArgumentException(e);
+        }
+        return element.getAnnotation(annotationType.asSubclass(Annotation.class));
+    }
+
+}
