3293dbb2c4a8ce22e22f01eaafa5e4da1c1fa87b
WashingtonWang <xiangyu1034.wang@gamil.com>
2017-10-25 16:18:48 +0800

    add a little of code

--- javabase/src/main/java/com/mycoding/javabase/JavabaseApplication.java
+++ javabase/src/main/java/com/mycoding/javabase/JavabaseApplication.java
@@ -1,13 +1,21 @@
 package com.mycoding.javabase;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavabaseApplication {
 
     public static void main(String[] args) {
-        System.out.println("12312");
-        SpringApplication.run(JavabaseApplication.class, args);
+        //System.out.println("12312");
+        //SpringApplication.run(JavabaseApplication.class, args);
+        //String a="abc";
+        //String b="abc";
+        //String c=new String("abc");
+        //System.out.println("a.hashCode="+a.hashCode());
+        //System.out.println("b.hashCode="+b.hashCode());
+        //System.out.println("c.hashCode="+c.hashCode());
+        //592242234
+        //System.out.println(-1 >>> 1);
+        //System.out.println(x010000);
     }
 }
