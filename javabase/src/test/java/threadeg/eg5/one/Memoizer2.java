8d16670998e3bb30d285ffc097e691bafe7a86fe
WashingtonWang <xiangyu1034.wang@gmail.com>
2017-11-07 23:45:30 +0800

    add new codes

--- javabase/src/test/java/threadeg/eg5/one/Memoizer2.java
+++ javabase/src/test/java/threadeg/eg5/one/Memoizer2.java
@@ -4,18 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description:  使用HashMap和同步机制来初始化缓存 《java并发编程实战》 P.86
- * 注意：用HashMap 存储缓存 synchronized用在方法上 这样的形式性能非常不好 (no)
+ * @Description:  使用HashMap和同步机制来初始化缓存 《java并发编程实战》 P.87
+ * 注意：将HashMap换成ConcurrentMap 把synchronized去掉
+ * 这样的性能会好些，但是这样存在一个漏洞，当两个线程同时调用ompute时可能会导致计算得到相同的值 (no)
  *
  * @Auther: wangxiangyu
  * @Date: 2017/11/6 21:54
  */
-public class Memoizer1<A,V> implements Computable<A,V> {
+public class Memoizer2<A,V> implements Computable<A,V> {
 
     private final Map<A, V> cache = new HashMap<>();
     private final Computable<A, V> c;
 
-    public Memoizer1(Computable<A, V> c) {
+    public Memoizer2(Computable<A, V> c) {
         this.c = c;
     }
 
