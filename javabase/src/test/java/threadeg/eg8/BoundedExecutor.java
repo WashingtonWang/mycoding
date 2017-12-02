ef3de2159a1211b8c21e7f42f4309f6eea0f5b0e
WashingtonWang <xiangyu1034.wang@gamil.com>
2017-12-02 18:24:54 +0800

    add new codes about ThreadExecutor

--- javabase/src/test/java/threadeg/eg8/BoundedExecutor.java
+++ javabase/src/test/java/threadeg/eg8/BoundedExecutor.java
@@ -1,8 +1,38 @@
 package threadeg.eg8;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+
 /**
+ * @Description: 使用Semaphore来控制任务的提交速率  《java并发编程实战》  P.145
  * user: xiangyu.wang
  * date: 2017/12/2 9:43
  */
 public class BoundedExecutor {
+    private final Executor exec;
+    private final Semaphore semaphore;
+
+    public BoundedExecutor(Executor exec, int bound){
+        this.exec = exec;
+        this.semaphore = new Semaphore(bound);
+    }
+
+    public void submitTask(final Runnable command) throws InterruptedException{
+        semaphore.acquire();
+        try {
+            exec.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        command.run();
+                    }finally {
+                        semaphore.release();
+                    }
+                }
+            });
+        }catch (RejectedExecutionException e){
+            semaphore.release();
+        }
+    }
 }
