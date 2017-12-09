9c721df5bb83e8b98299d5a44521fb603b3bab75
WashingtonWang <xiangyu1034.wang@gamil.com>
2017-12-09 16:37:30 +0800

    add new codes about innerClass <think in java>

---
+++ javabase/src/test/java/innerclass/eg2/Checkers.java
@@ -0,0 +1,31 @@
+package innerclass.eg2;
+
+/**
+ * user: xiangyu.wang
+ * date: 2017/12/8 18:16
+ */
+public class Checkers implements Game {
+
+    private Checkers(){}
+
+    private int moves = 0;
+    private static final int MOVES = 3;
+
+    @Override
+    public boolean move() {
+        System.out.println("Checkers move " + moves);
+        return ++moves != MOVES;
+    }
+
+    //lamdba形式
+    public static GameFactory factory = () -> new Checkers();
+
+    //内部类形式
+    public static GameFactory factory1 =
+            new GameFactory() {
+                @Override
+                public Game getGame() {
+                    return new Checkers();
+                }
+            };
+}
---
+++ javabase/src/test/java/innerclass/eg2/GameFactory.java
@@ -0,0 +1,9 @@
+package innerclass.eg2;
+
+/**
+ * user: xiangyu.wang
+ * date: 2017/12/8 18:16
+ */
+public interface GameFactory {
+    Game getGame();
+}
