194950e9919b10e5009fa8867e5e84f0df9db88f
WashingtonWang <xiangyu1034.wang@gamil.com>
2018-01-20 17:17:38 +0800

    modify application.java

--- springframework-base-learn/src/main/java/com/mycoding/springframeworkbaselearn/SpringframeworkBaseLearnApplication.java
+++ springframework-base-learn/src/main/java/com/mycoding/springframeworkbaselearn/SpringframeworkBaseLearnApplication.java
@@ -1,13 +1,7 @@
 package com.mycoding.springframeworkbaselearn;
 
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContextAware;
 
 /**
  * @author xiangyu.wang
