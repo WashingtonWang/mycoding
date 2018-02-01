49ab556956f8e58676489f354d233c485ba39fe1
WashingtonWang <xiangyu1034.wang@gamil.com>
2018-02-01 19:54:33 +0800

    modify codes

--- javabase/src/test/java/annotationeg/eg1/Sample2.java
+++ javabase/src/test/java/annotationeg/eg1/Sample2.java
@@ -5,12 +5,16 @@ public class Sample2 {
     public static void m1(){
         int i = 0;
         i = i / i;
+        System.out.println(i);
     }
     @ExceptionTest(ArithmeticException.class)
     public static void m2(){
         int[] a= new int[10];
         int i = a[1];
+        System.out.println(i);
     }
     @ExceptionTest(ArithmeticException.class)
-    public static void m3(){    }
+    public static void m3(){
+        System.out.println("i am m3!");
+    }
 }
